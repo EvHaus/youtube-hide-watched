0161d7f9b492b504d269b71103b7da54a3d201c0
Evgueni Naverniouk <info@globexdesigns.com>
2016-11-26 14:23:58 -0800

    v1.0
    Fixes #4

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube: Hide Watched Videos
 // @namespace    http://www.globexdesigns.com/
-// @version      0.7
+// @version      1.0
 // @description  Hides watched videos from your YouTube subscriptions page.
 // @author       Evgueni Naverniouk
 // @grant        GM_addStyle
@@ -17,31 +17,118 @@
 // https://github.com/globexdesigns/youtube-hide-watched/issues
 
 (function (undefined) {
+    // Enable for debugging
+    var __DEV__ = true;
 
-    GM_addStyle("\
-.GR-WATCHED {\
-display: none;\
-}\
-.GR-BUTTON {\
-cursor: pointer;\
-position: absolute;\
-right: 80px;\
-top: 10px;\
-}\
-.GR-BUTTON-CHECKBOX {\
-pointer-events: none;\
-}\
-");
+    // Set defaults
+    localStorage.YTHWV_WATCHED = localStorage.YTHWV_WATCHED || 'false';
+    localStorage.YTHWV_WATCH_PERC = localStorage.YTHWV_WATCH_PERC || '0';
+
+    GM_addStyle(`
+.YT-HWV-WATCHED { display: none !important; }
+
+.YT-HWV-CONTAINER {
+    display: inline-flex;
+    position: relative;
+    vertical-align: -2px;
+}
+
+.YT-HWV-BUTTON {
+    align-items: center;
+    background: #F8F8F8;
+    border: 1px solid #D3D3D3;
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.05);
+    color: #333;
+    cursor: pointer;
+    display: flex;
+    font-size: 11px;
+    font-weight: 500;
+    height: 28px;
+}
+
+.YT-HWV-BUTTON:focus,
+.YT-HWV-BUTTON:hover {
+    background: #F0F0F0;
+    border-color: #C6C6C6;
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.10);
+}
+
+.YT-HWV-HIDEBUTTON {
+    border-radius: 2px 0 0 2px;
+    padding: 0 10px;
+}
+
+.YT-HWV-MENUBUTTON {
+    border-radius: 0 2px 2px 0;
+    border-left: 0;
+    padding: 0 10px 0 5px;
+}
+
+.YT-HWV-BUTTON-CHECKBOX {
+    margin: 0 8px 0 0;
+    pointer-events: none;
+    vertical-align: 2px;
+}
+
+.YT-HWV-MENU {
+    background: #F8F8F8;
+    border: 1px solid #D3D3D3;
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.05);
+    display: none;
+    font-size: 12px;
+    margin-top: -1px;
+    padding: 10px;
+    position: absolute;
+    right: 0;
+    text-align: center;
+    top: 100%;
+    white-space: normal;
+    z-index: 9999;
+}
+
+.YT-HWV-MENU-ON { display: block; }
+.YT-HWV-MENUBUTTON-ON span { transform: rotate(180deg) }
+
+.YT-HWV-MENU-WATCH-PERC {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    font-size: 11px;
+    margin: 5px auto;
+}
+
+.YT-HWV-MENU-WATCH-PERC-INPUT {
+    cursor: pointer;
+    margin: auto 10px;
+    vertical-align: -4px;
+}
+`);
+
+    // ===========================================================
+
+    var addJQuery = function (callback) {
+        var script = document.createElement("script");
+        script.setAttribute("src", "//code.jquery.com/jquery-3.1.1.slim.min.js");
+        script.setAttribute("integrity", "sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=");
+        script.setAttribute("crossorigin", "anonymous");
+        script.addEventListener('load', function() {
+            document.body.appendChild(script);
+            if (__DEV__) console.log('[YT-HWV] jQuery has been loaded');
+            callback();
+        }, false);
+        document.body.appendChild(script);
+    };
 
     // ===========================================================
 
     var findWatchedElements = function () {
-        var bars = document.getElementsByClassName('resume-playback-progress-bar');
-        var watched = [];
-        for (var i = 0, l = bars.length; i < l; i++) {
-            var bar = bars[i];
-            if (bar.style.width && parseInt(bar.style.width, 10) > 0) watched.push(bar);
-        }
+        var watched = $('.resume-playback-progress-bar')
+            .filter(function (i, bar) {
+                return bar.style.width && parseInt(bar.style.width, 10) > parseInt(localStorage.YTHWV_WATCH_PERC, 10);
+            });
+
+        if (__DEV__) console.log(`[YT-HWV] Found ${watched.length} watched elements`);
+
         return watched;
     };
 
@@ -55,45 +142,59 @@ pointer-events: none;\
     // ===========================================================
 
     var findButtonTarget = function () {
-        return document.getElementById('browse-items-primary');
+        // Button will be injected into the menu of an item browser
+        var target = $('#browse-items-primary .yt-uix-menu-top-level-button-container');
+
+        // If this is a "History" video -- we don't need a button. We use
+        // DOM detection here instead of URL detection, because the URL
+        // will change before the DOM has been updated.
+        if ($('#watch-history-pause-button').length > 0) return;
+
+        return target;
     };
 
     // ===========================================================
 
     var isButtonAlreadyThere = function () {
-        var button = document.getElementsByClassName('GR-BUTTON');
-        return button && button.length;
+        return $('.YT-HWV-CONTAINER').length > 0;
     };
 
     // ===========================================================
 
     var addClassToWatchedRows = function () {
-        var items = findWatchedElements() || [];
-        for (var i = 0, l = items.length; i < l; i++) {
-            var item = items[i];
+        // Clean up first
+        $('.YT-HWV-WATCHED').removeClass('YT-HWV-WATCHED');
 
+        if (localStorage.YTHWV_WATCHED !== 'true') return;
+
+        $(findWatchedElements()).each(function (i, item) {
             // "Subscription" section needs us to hide the "feed-item-container",
             // but in the "Trending" section, that class will hide everything.
             // So there, we need to hide the "expanded-shelf-content-item-wrapper"
             var row;
             if (window.location.href.indexOf('/feed/subscriptions') > 0) {
-                row = findParentByClass(item, 'feed-item-container');
+                row = item.closest('.feed-item-container');
             } else {
-                row = findParentByClass(item, 'expanded-shelf-content-item-wrapper');
+                row = item.closest('.expanded-shelf-content-item-wrapper');
             }
 
-            var gridItem = findParentByClass(item, 'yt-shelf-grid-item');
+            var gridItem = item.closest('.yt-shelf-grid-item');
 
             // If we're in grid view, we will hide the "grid" item,
             // otherwise we'll hide the item row
-            var itemToHide = gridItem ? gridItem : row;
+            var itemsToHide = gridItem ? $(gridItem) : $(row);
 
-            if (localStorage.GRWATCHED === 'true') {
-                itemToHide.classList.add('GR-WATCHED');
-            } else {
-                itemToHide.classList.remove('GR-WATCHED');
+            // If this is the first row in the list, then we can't hide it entirely,
+            // otherwise it will also hide the menu. So, we'll have to hide various
+            // inner components instead.
+            const hasMenu = itemsToHide.find('.menu-container.shelf-title-cell .yt-uix-menu-container').length > 0;
+            if (hasMenu) {
+                var itemToHide = itemsToHide;
+                itemsToHide = itemToHide.find('.expanded-shelf').add(itemToHide.find('.branded-page-module-title'));
             }
-        }
+
+            itemsToHide.addClass('YT-HWV-WATCHED');
+        });
     };
 
     // ===========================================================
@@ -105,34 +206,44 @@ pointer-events: none;\
         var target = findButtonTarget();
         if (!target) return;
 
-        // Add checkbox
-        var checkboxContainer = document.createElement('span');
-        checkboxContainer.className = 'yt-uix-button-icon-wrapper';
-        var checkbox = document.createElement('input');
-        checkbox.checked = localStorage.GRWATCHED === 'true' ? 'checked' : null;
-        checkbox.className = 'GR-BUTTON-CHECKBOX';
-        checkbox.type = 'checkbox';
-        checkboxContainer.appendChild(checkbox);
+        // Generate button DOM
+        var li = $('<li class="yt-uix-menu-top-level-button yt-uix-menu-top-level-flow-button" />');
+        var container = $('<div class="YT-HWV-CONTAINER" />').appendTo(li);
+        var button = $('<button class="YT-HWV-BUTTON YT-HWV-HIDEBUTTON">Hide Watched</button>').appendTo(container);
+        var checkbox = $('<input class="YT-HWV-BUTTON-CHECKBOX" type="checkbox" />').prependTo(button);
+        var menubutton = $('<button class="YT-HWV-BUTTON YT-HWV-MENUBUTTON"><span class="yt-uix-button-arrow yt-sprite" /></button>').appendTo(container);
+        var menu = $('<div class="YT-HWV-MENU">Videos are considered "watched" when you have watched at least: </div>').appendTo(container);
+        var watchedContainer = $('<div class="YT-HWV-MENU-WATCH-PERC">0%<span />100%</div>').appendTo(menu);
+        var watchedInput = $('<input class="YT-HWV-MENU-WATCH-PERC-INPUT" type="range" max="100" min="0" />').appendTo(menu.find('span'));
 
-        // Add label text
-        var textnode = document.createTextNode("Hide Watched");
-
-        // Create <button>
-        var button = document.createElement('button');
-        button.className = 'yt-uix-button yt-uix-button-size-default yt-uix-button-default GR-BUTTON';
-        button.appendChild(checkboxContainer);
-        button.appendChild(textnode);
-        button.addEventListener('click', function (event) {
-            var value = localStorage.GRWATCHED === 'true' ? 'false' : 'true';
-            localStorage.GRWATCHED = value;
-            checkbox.checked = value === 'true';
+        // Attach events
+        button.on("click", function () {
+            var value = localStorage.YTHWV_WATCHED === 'true' ? 'false' : 'true';
+            localStorage.YTHWV_WATCHED = value;
+            checkbox.attr('checked', value === 'true' ? true : false);
             addClassToWatchedRows();
         });
 
-        target.appendChild(button);
+        menubutton.on("click", function () {
+            menubutton.toggleClass("YT-HWV-MENUBUTTON-ON");
+            menu.toggleClass("YT-HWV-MENU-ON");
+        });
+
+        watchedInput.on("change", function (event) {
+            localStorage.YTHWV_WATCH_PERC = event.target.value;
+            run();
+        });
+
+        // Set DOM values accordingly
+        if (localStorage.YTHWV_WATCHED === 'true') checkbox.attr('checked', true);
+        watchedInput.attr('value', localStorage.YTHWV_WATCH_PERC);
+
+        // Insert button into DOM
+        target.prepend(li);
     };
 
     var run = function () {
+        if (__DEV__) console.log('[YT-HWV] Running check for watched videos');
         addClassToWatchedRows();
         addCheckboxButton();
     };
@@ -157,34 +268,37 @@ pointer-events: none;\
 
     // ===========================================================
 
-    var observeDOM = (function(){
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-            eventListenerSupported = window.addEventListener;
+    var observeDOM = (function() {
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        var eventListenerSupported = window.addEventListener;
 
-        return function(obj, callback){
-            if( MutationObserver ){
-                // define a new observer
-                var obs = new MutationObserver(function(mutations, observer){
-                    if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
-                        callback();
+        return function(obj, callback) {
+            if (MutationObserver) {
+                var obs = new MutationObserver(function (mutations, observer) {
+                    if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+                        callback(mutations);
+                    }
                 });
-                // have the observer observe foo for changes in children
-                obs.observe( obj, { childList:true, subtree:true });
-            }
-            else if( eventListenerSupported ){
+
+                obs.observe(obj, {childList: true, subtree: true});
+            } else if (eventListenerSupported) {
                 obj.addEventListener('DOMNodeInserted', callback, false);
                 obj.addEventListener('DOMNodeRemoved', callback, false);
             }
         };
     })();
 
-    // YouTube does weird things during navigation. This seems to be the only reliable way to
-    // check when user moves from one page to another.
-    observeDOM(document.body, function () {
-        run();
-    });
-
     // ===========================================================
 
-    run();
+    if (__DEV__) console.log('[YT-HWV] Starting Script');
+    addJQuery(function () {
+        // YouTube does navigation via history and also does a bunch
+        // of AJAX video loading. In order to ensure we're always up
+        // to date, we have to listen for ANY DOM change event, and
+        // re-run our script.
+        if (__DEV__) console.log('[YT-HWV] Attaching DOM listener');
+        observeDOM(document.body, run);
+
+        run();
+    });
 }());
