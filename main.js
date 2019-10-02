cadc5b198d7b58d2cf8df431ea2a828db5053353
Ev Haus <ev@zenhub.com>
2019-10-01 19:37:34 -0700

    v2.9.0 Add ESLint

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         YouTube: Hide Watched Videos
 // @namespace    http://www.globexdesigns.com/
-// @version      2.8.1
-// @description  Hides watched videos from your YouTube subscriptions page.
+// @version      2.9.0
+// @description  Hides watched videos from your YouTube subscriptions page
 // @author       Ev Haus
 // @include      http://*.youtube.com/*
 // @include      http://youtube.com/*
@@ -12,45 +12,46 @@
 // ==/UserScript==
 
 // To submit bugs or submit revisions please see visit the repository at:
-// https://github.com/globexdesigns/youtube-hide-watched
+// https://github.com/EvHaus/youtube-hide-watched
 // You can open new issues at:
-// https://github.com/globexdesigns/youtube-hide-watched/issues
+// https://github.com/EvHaus/youtube-hide-watched/issues
 
+// eslint-disable-next-line no-shadow-restricted-names
 (function (undefined) {
-    
-    // ===================================================================
-    // How much of the video needs to be watched before it will be hidden?
-    // 
-    // For example, when set to 10 the video will *not* be hidden until 
-    // you've watched at least 10 percent of the video.	
-    //
-    // If you set to 0 (zero) percent, then if you watch even one second 
-    // of the video, it will be hidden.
-    var HiddenThresholdPercent = 10;
-    
-    //====================================================================
-    
-    // Enable for debugging
-    var __DEV__ = false;
 
-    // Set defaults
-    localStorage.YTHWV_WATCHED = localStorage.YTHWV_WATCHED || 'false';
-  
-  	// GreaseMonkey no longer supports GM_addStyle. So we have to define
-    // our own polyfill here
-  	var addStyle = function (aCss) {
-    	let head = document.getElementsByTagName('head')[0];
-      if (head) {
-        let style = document.createElement('style');
-        style.setAttribute('type', 'text/css');
-        style.textContent = aCss;
-        head.appendChild(style);
-        return style;
-      }
-      return null;
-    };
+	// ===================================================================
+	// How much of the video needs to be watched before it will be hidden?
+	//
+	// For example, when set to 10 the video will *not* be hidden until
+	// you've watched at least 10 percent of the video.
+	//
+	// If you set to 0 (zero) percent, then if you watch even one second
+	// of the video, it will be hidden.
+	const HiddenThresholdPercent = 10;
 
-    addStyle(`
+	// ====================================================================
+
+	// Enable for debugging
+	const __DEV__ = false;
+
+	// Set defaults
+	localStorage.YTHWV_WATCHED = localStorage.YTHWV_WATCHED || 'false';
+
+	// GreaseMonkey no longer supports GM_addStyle. So we have to define
+	// our own polyfill here
+	const addStyle = function (aCss) {
+		const head = document.getElementsByTagName('head')[0];
+		if (head) {
+			const style = document.createElement('style');
+			style.setAttribute('type', 'text/css');
+			style.textContent = aCss;
+			head.appendChild(style);
+			return style;
+		}
+		return null;
+	};
+
+	addStyle(`
 .YT-HWV-WATCHED {
     display: none !important;
 }
@@ -96,206 +97,206 @@ html[dark] .YT-HWV-BUTTON {
 .YT-HWV-MENUBUTTON-ON span { transform: rotate(180deg) }
 `);
 
-    var visibilityIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="currentColor"><path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z"/></g></svg>';
-    var visibilityOffIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="currentColor"><path d="M24 14c5.52 0 10 4.48 10 10 0 1.29-.26 2.52-.71 3.65l5.85 5.85c3.02-2.52 5.4-5.78 6.87-9.5-3.47-8.78-12-15-22.01-15-2.8 0-5.48.5-7.97 1.4l4.32 4.31c1.13-.44 2.36-.71 3.65-.71zM4 8.55l4.56 4.56.91.91C6.17 16.6 3.56 20.03 2 24c3.46 8.78 12 15 22 15 3.1 0 6.06-.6 8.77-1.69l.85.85L39.45 44 42 41.46 6.55 6 4 8.55zM15.06 19.6l3.09 3.09c-.09.43-.15.86-.15 1.31 0 3.31 2.69 6 6 6 .45 0 .88-.06 1.3-.15l3.09 3.09C27.06 33.6 25.58 34 24 34c-5.52 0-10-4.48-10-10 0-1.58.4-3.06 1.06-4.4zm8.61-1.57l6.3 6.3L30 24c0-3.31-2.69-6-6-6l-.33.03z"/></g></svg>';
+	// eslint-disable-next-line max-len
+	const visibilityIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="currentColor"><path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z"/></g></svg>';
 
-    // ===========================================================
+	// eslint-disable-next-line max-len
+	const visibilityOffIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="currentColor"><path d="M24 14c5.52 0 10 4.48 10 10 0 1.29-.26 2.52-.71 3.65l5.85 5.85c3.02-2.52 5.4-5.78 6.87-9.5-3.47-8.78-12-15-22.01-15-2.8 0-5.48.5-7.97 1.4l4.32 4.31c1.13-.44 2.36-.71 3.65-.71zM4 8.55l4.56 4.56.91.91C6.17 16.6 3.56 20.03 2 24c3.46 8.78 12 15 22 15 3.1 0 6.06-.6 8.77-1.69l.85.85L39.45 44 42 41.46 6.55 6 4 8.55zM15.06 19.6l3.09 3.09c-.09.43-.15.86-.15 1.31 0 3.31 2.69 6 6 6 .45 0 .88-.06 1.3-.15l3.09 3.09C27.06 33.6 25.58 34 24 34c-5.52 0-10-4.48-10-10 0-1.58.4-3.06 1.06-4.4zm8.61-1.57l6.3 6.3L30 24c0-3.31-2.69-6-6-6l-.33.03z"/></g></svg>';
 
-    var debounce = function (func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    };
+	// ===========================================================
 
-    // ===========================================================
+	const debounce = function (func, wait, immediate) {
+		let timeout;
+		return (...args) => {
+			const later = () => {
+				timeout = null;
+				if (!immediate) func.apply(this, args);
+			};
+			const callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(this, args);
+		};
+	};
 
-    var findWatchedElements = function () {
-        var watched = $('.ytd-thumbnail-overlay-resume-playback-renderer');
+	// ===========================================================
 
-        if (__DEV__) console.log(`[YT-HWV] Found ${watched.length} watched elements`);
+	const findWatchedElements = function () {
+		const watched = $('.ytd-thumbnail-overlay-resume-playback-renderer');
 
-        return watched.filter(function (i, bar) {
-            return bar.style.width && parseInt(bar.style.width, 10) > HiddenThresholdPercent;
-        });
-    };
+		// eslint-disable-next-line no-console
+		if (__DEV__) console.log(`[YT-HWV] Found ${watched.length} watched elements`);
 
-    // ===========================================================
+		return watched.filter((i, bar) => {
+			return bar.style.width && parseInt(bar.style.width, 10) > HiddenThresholdPercent;
+		});
+	};
 
-    var findParentByClass = function(el, cls) {
-        while ((el = el.parentElement) && !el.classList.contains(cls));
-        return el;
-    };
+	// ===========================================================
 
-    // ===========================================================
+	const findButtonTarget = function () {
+		// Button will be injected into the main header menu
+		return $('#container #end #buttons');
+	};
 
-    var findButtonTarget = function () {
-        // Button will be injected into the main header menu
-        return $('#container #end #buttons');
-    };
+	// ===========================================================
 
-    // ===========================================================
+	const isButtonAlreadyThere = function () {
+		return $('.YT-HWV-BUTTON').length > 0;
+	};
 
-    var isButtonAlreadyThere = function () {
-        return $('.YT-HWV-BUTTON').length > 0;
-    };
+	// ===========================================================
 
-    // ===========================================================
+	const addClassToWatchedRows = function () {
+		if (localStorage.YTHWV_WATCHED !== 'true') return;
 
-    var addClassToWatchedRows = function () {
-        if (localStorage.YTHWV_WATCHED !== 'true') return;
+		// If we're on the HIstory page -- do nothing. We don't want to hide
+		// watched videos here.
+		if (window.location.href.indexOf('/feed/history') >= 0) return;
 
-        // If we're on the HIstory page -- do nothing. We don't want to hide
-        // watched videos here.
-        if (window.location.href.indexOf('/feed/history') >= 0) return null;
+		// [TODO] If we're on the Trending page -- we don't support it yet.
+		if (window.location.href.indexOf('/feed/trending') >= 0) return;
 
-        // [TODO] If we're on the Trending page -- we don't support it yet.
-        if (window.location.href.indexOf('/feed/trending') >= 0) return null;
+		$(findWatchedElements()).each((i, item) => {
+			// "Subscription" section needs us to hide the "#contents",
+			// but in the "Trending" section, that class will hide everything.
+			// So there, we need to hide the "ytd-video-renderer"
+			let gridItem, row;
+			if (window.location.href.indexOf('/feed/subscriptions') > 0) {
+				// For rows, hide the row and the header too. We can't hide
+				// their entire parent because then we'll get the infinite
+				// page loader to load forever.
+				row = item.closest('#grid-container');
+				row = $(row).add($(item.closest('#dismissable.ytd-shelf-renderer')).children('.grid-subheader'));
 
-        $(findWatchedElements()).each(function (i, item) {
-            // "Subscription" section needs us to hide the "#contents",
-            // but in the "Trending" section, that class will hide everything.
-            // So there, we need to hide the "ytd-video-renderer"
-            var row, gridItem;
-            if (window.location.href.indexOf('/feed/subscriptions') > 0) {
-                // For rows, hide the row and the header too. We can't hide
-                // their entire parent because then we'll get the infinite
-                // page loader to load forever.
-                row = item.closest('#grid-container');
-                row = $(row).add($(item.closest('#dismissable.ytd-shelf-renderer')).children('.grid-subheader'));
+				gridItem = item.closest('.ytd-grid-renderer');
+			} else if (window.location.href.match(/.*\/(user|channel)\/.+\/videos/u)) {
+				// Channel "Videos" section needs special handling
+				row = item.closest('.ytd-grid-renderer');
+			} else {
+				row = item.closest('ytd-video-renderer');
+			}
 
-                gridItem = item.closest('.ytd-grid-renderer');
-            } else if (window.location.href.match(/.*\/(user|channel)\/.+\/videos/)) {
-                // Channel "Videos" section needs special handling
-                row = item.closest('.ytd-grid-renderer');
-            } else {
-                row = item.closest('ytd-video-renderer');
-            }
+			// If we're in grid view, we will hide the "grid" item,
+			// otherwise we'll hide the item row
+			let itemsToHide = gridItem ? $(gridItem) : $(row);
 
-            // If we're in grid view, we will hide the "grid" item,
-            // otherwise we'll hide the item row
-            var itemsToHide = gridItem ? $(gridItem) : $(row);
+			// If this is the first row in the list, then we can't hide it entirely,
+			// otherwise it will also hide the menu. So, we'll have to hide various
+			// inner components instead.
+			const hasMenu = itemsToHide.find('.menu-container.shelf-title-cell .yt-uix-menu-container').length > 0;
+			if (hasMenu) {
+				const itemToHide = itemsToHide;
+				itemsToHide = itemToHide.find('.expanded-shelf').add(itemToHide.find('.branded-page-module-title'));
+			}
 
-            // If this is the first row in the list, then we can't hide it entirely,
-            // otherwise it will also hide the menu. So, we'll have to hide various
-            // inner components instead.
-            const hasMenu = itemsToHide.find('.menu-container.shelf-title-cell .yt-uix-menu-container').length > 0;
-            if (hasMenu) {
-                var itemToHide = itemsToHide;
-                itemsToHide = itemToHide.find('.expanded-shelf').add(itemToHide.find('.branded-page-module-title'));
-            }
+			itemsToHide.addClass('YT-HWV-WATCHED');
+		});
+	};
 
-            itemsToHide.addClass('YT-HWV-WATCHED');
-        });
-    };
+	// ===========================================================
 
-    // ===========================================================
+	const removeClassFromWatchedRows = function () {
+		$('.YT-HWV-WATCHED').each((i, item) => {
+			$(item).removeClass('YT-HWV-WATCHED');
+		});
+	};
 
-    var removeClassFromWatchedRows = function () {
-        $('.YT-HWV-WATCHED').each(function (i, item) {
-           $(item).removeClass('YT-HWV-WATCHED');
-        });
-    };
+	// ===========================================================
 
-    // ===========================================================
+	const addButton = function () {
+		if (isButtonAlreadyThere()) return;
 
-    var addButton = function () {
-        if (isButtonAlreadyThere()) return;
+		// Find button target
+		const target = findButtonTarget();
+		if (!target) return;
 
-        // Find button target
-        var target = findButtonTarget();
-        if (!target) return;
+		// Generate button DOM
+		const icon = localStorage.YTHWV_WATCHED === 'true' ? visibilityIcon : visibilityOffIcon;
+		const button = $(`<button class="YT-HWV-BUTTON" title="Toggle Watched Videos">${icon}</button>`);
 
-        // Generate button DOM
-        var icon = localStorage.YTHWV_WATCHED === 'true' ? visibilityIcon : visibilityOffIcon;
-        var button = $('<button class="YT-HWV-BUTTON" title="Toggle Watched Videos">' + icon + '</button>');
+		// Attach events
+		button.on('click', function () {
+			const value = localStorage.YTHWV_WATCHED === 'true' ? 'false' : 'true';
+			localStorage.YTHWV_WATCHED = value;
+			if (value === 'true') {
+				addClassToWatchedRows();
+				$(this).html(visibilityIcon);
+			} else {
+				removeClassFromWatchedRows();
+				$(this).html(visibilityOffIcon);
+			}
+		});
 
-        // Attach events
-        button.on("click", function () {
-            var value = localStorage.YTHWV_WATCHED === 'true' ? 'false' : 'true';
-            localStorage.YTHWV_WATCHED = value;
-            if (value === 'true') {
-                addClassToWatchedRows();
-                $(this).html(visibilityIcon);
-            } else {
-                removeClassFromWatchedRows();
-                $(this).html(visibilityOffIcon);
-            }
-        });
+		// Insert button into DOM
+		target.prepend(button);
+	};
 
-        // Insert button into DOM
-        target.prepend(button);
-    };
+	const run = debounce(() => {
+		// eslint-disable-next-line no-console
+		if (__DEV__) console.log('[YT-HWV] Running check for watched videos');
+		removeClassFromWatchedRows();
+		addClassToWatchedRows();
+		addButton();
+	}, 250);
 
-    var run = debounce(function () {
-        if (__DEV__) console.log('[YT-HWV] Running check for watched videos');
-        removeClassFromWatchedRows();
-        addClassToWatchedRows();
-        addButton();
-    }, 250);
+	// ===========================================================
 
-    // ===========================================================
+	// Hijack all XHR calls
+	const send = XMLHttpRequest.prototype.send;
+	XMLHttpRequest.prototype.send = function (data) {
+		this.addEventListener('readystatechange', function () {
+			if (
+			// Anytime more videos are fetched -- re-run script
+				this.responseURL.indexOf('browse_ajax?action_continuation') > 0
+			) {
+				setTimeout(() => {
+					run();
+				}, 0);
+			}
+		}, false);
+		send.call(this, data);
+	};
 
-    // Hijack all XHR calls
-    var send = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function (data) {
-        this.addEventListener("readystatechange", function () {
-            if (
-                // Anytime more videos are fetched -- re-run script
-                this.responseURL.indexOf('browse_ajax?action_continuation') > 0
-            ) {
-                setTimeout(function () {
-                    run();
-                }, 0);
-            }
-        }, false);
-        send.call(this, data);
-    };
+	// ===========================================================
 
-    // ===========================================================
+	const observeDOM = (function () {
+		const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		const eventListenerSupported = window.addEventListener;
 
-    var observeDOM = (function() {
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-        var eventListenerSupported = window.addEventListener;
+		return function (obj, callback) {
+			// eslint-disable-next-line no-console
+			if (__DEV__) console.log('[YT-HWV] Attaching DOM listener');
 
-        return function(obj, callback) {
-            if (__DEV__) console.log('[YT-HWV] Attaching DOM listener');
+			// Invalid `obj` given
+			if (!obj) return;
 
-            // Invalid `obj` given
-            if (!obj) return;
+			if (MutationObserver) {
+				const obs = new MutationObserver(((mutations, observer) => {
+					if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+						// eslint-disable-next-line callback-return
+						callback(mutations);
+					}
+				}));
 
-            if (MutationObserver) {
-                var obs = new MutationObserver(function (mutations, observer) {
-                    if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
-                        callback(mutations);
-                    }
-                });
+				obs.observe(obj, {childList: true, subtree: true});
+			} else if (eventListenerSupported) {
+				obj.addEventListener('DOMNodeInserted', callback, false);
+				obj.addEventListener('DOMNodeRemoved', callback, false);
+			}
+		};
+	}());
 
-                obs.observe(obj, {childList: true, subtree: true});
-            } else if (eventListenerSupported) {
-                obj.addEventListener('DOMNodeInserted', callback, false);
-                obj.addEventListener('DOMNodeRemoved', callback, false);
-            }
-        };
-    })();
+	// ===========================================================
 
-    // ===========================================================
+	// eslint-disable-next-line no-console
+	if (__DEV__) console.log('[YT-HWV] Starting Script');
 
-    if (__DEV__) console.log('[YT-HWV] Starting Script');
+	// YouTube does navigation via history and also does a bunch
+	// of AJAX video loading. In order to ensure we're always up
+	// to date, we have to listen for ANY DOM change event, and
+	// re-run our script.
+	observeDOM(document.body, run);
 
-    // YouTube does navigation via history and also does a bunch
-    // of AJAX video loading. In order to ensure we're always up
-    // to date, we have to listen for ANY DOM change event, and
-    // re-run our script.
-    observeDOM(document.body, run);
-
-    run();
+	run();
 }());
