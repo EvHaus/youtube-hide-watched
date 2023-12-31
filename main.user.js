081a60216bc95358b9413a2828292ae7ce1a4af6
Ev Haus <ev@haus.gg>
2023-12-30 19:04:14 -0800

    v6.3 version up

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube: Hide Watched Videos
 // @namespace    https://www.haus.gg/
-// @version      6.2
+// @version      6.3
 // @license      MIT
 // @description  Hides watched videos (and shorts) from your YouTube subscriptions page.
 // @author       Ev Haus
